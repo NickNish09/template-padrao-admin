b938b10c1ccfe88c764121457b52c598ff7a3852
NickNish09 <nnmarques97@gmail.com>
2018-10-27 03:07:09 -0300

    importados assets de star admin

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,6 +51,7 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  # config.assets.precompile = ['*.js', '*.css', '*.jpg', '*.png']
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
